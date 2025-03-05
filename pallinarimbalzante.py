f4a6933f652d7491c1e990dd9fd1b80cc146041f
gardella@elocal.it <gardella@elocal.it>
2025-03-05 18:47:33 +0100

    pallina rimbalzante

--- pallinarimbalzante.py
+++ pallinarimbalzante.py
@@ -3,23 +3,28 @@ import numpy as np
 
 sciatore = cv2.imread('sciatore.jpg')
 
+
+
 yMax, xMax = sciatore.shape[:2]
-raggio = 10
+raggio = 5
 _quit = False
 x= xMax //2 - raggio
 y= yMax //2 - raggio
 deltaX = 1
 deltaY = 1
 
-pallina = np.ones((raggio*2,raggio*2,3), dtype=np.uint8)
-cv2.circle(pallina,(raggio,raggio),raggio,(0,0,255),3)
+pallina = np.ones((raggio*2+2,raggio*2+2,3), dtype=np.uint8)
+cv2.circle(pallina,(raggio+1,raggio+1),raggio,(0,0,255),1)
 yPallina, xPallina = pallina.shape[:2]
 
 
 while not _quit:
     
     frame = sciatore.copy()
-    frame[y:y+yPallina,x:x+xPallina,:] = pallina.copy()
+    for ypall in range (yPallina):
+        for xpall in range (xPallina):
+            if pallina[ypall,xpall,2] == 255:
+                frame[y+ypall,x+xpall,:] = pallina[ypall,xpall,:]
     
     if x ==0 or x+xPallina == xMax-1:
         deltaX = -deltaX
